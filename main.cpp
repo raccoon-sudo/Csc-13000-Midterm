d47b704d30ecd9ed619d658d1bc633f34f91b5c2
Daniel Arthur <107006568+MediocreProgrammer89@users.noreply.github.com>
2026-03-23 13:39:25 -0400

    Added core basics of decryption

--- main.cpp
+++ main.cpp
@@ -6,6 +6,32 @@
 using namespace std;
 
 int main(){
-    cout << "This works" << endl;
+    const string ENCRYPTEDTEXT = "TJXUN LWWTK VULPY EIPXB ADYMP TXNEY DGOPL "
+    "QHLEC VGKNO ZCGGK GUTPL LYVAC NRTRC CGXOH SYTGO YRHPI NQPBO "
+    "HEMOT WYRTE RORXL FZCKI VVUNQ ZJLQP UNLWW SKLVU ETRHC LHOAZ UETHZ AYEGC KIUTT " 
+    "YEAPR UNLWW TKVUL CPJAV BINDZ PTTNN HDTRS RKIVT YAEKL NOLDQ IUMOS TYKAM BHGLG YINTV "
+    "LPHFA VPYAC PYLTX UDJLA QWTCN EPBWK MBTSP YNCES TTNYL GGAIY PUECO YSLWE EDKUI NAYTV XLNDU "
+    "SSVTM TSPHA EJOAC OJOQF QELGC SHEIW PCQAP WFELG CS";
+
+    string choice;
+    /* We probably do not need to write all 96 keys into a vector so I stopped at the 55 item(suck)
+    vector<string> keys = {"receipt", "vengeful" , "popcorn", "basketball", "friends", "wealth", "interrupt",
+    "awesome", "actually", "kindhearted", "nifty", "change", "heat", "squeal", "treatment", "satisfy", "separate", "hot", "serious", 
+    "pail", "request", "request", "different", "righteous", "cat", "cheap", "repair", "uncle", "creepy", "birds", "crook", "great",
+    "ski", "stitch", "example", "flippant", "puzzling", "sleepy", "discover", "excuse", "unbiased", "jail", "eggs", "material",
+    "expand", "equal", "group", "bizzare", "spotted", "embarrass", "produce", "womanly", "spectacular", "agonizing", "cows", "suck"};
+    */
+
+    cout << "Would you like to decrypt the text(D) or encrypt your own(E). Please only enter E or D\n";
+    getline(cin , choice);
+
+
+    if(choice == "E"){
+
+    }
+    else{
+
+    }
+
     return 0;
 }
