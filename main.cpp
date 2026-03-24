e196177f8de35e28094d389ced391e2624539cdb
Tao <rent2209@gmail.com>
2026-03-24 05:22:31 +0000

    starting encrypting / decrypting

--- main.cpp
+++ main.cpp
@@ -6,32 +6,43 @@
 using namespace std;
 
 int main(){
-    const string ENCRYPTEDTEXT = "TJXUN LWWTK VULPY EIPXB ADYMP TXNEY DGOPL "
+    const string encryptedTEXT = "TJXUN LWWTK VULPY EIPXB ADYMP TXNEY DGOPL "
     "QHLEC VGKNO ZCGGK GUTPL LYVAC NRTRC CGXOH SYTGO YRHPI NQPBO "
     "HEMOT WYRTE RORXL FZCKI VVUNQ ZJLQP UNLWW SKLVU ETRHC LHOAZ UETHZ AYEGC KIUTT " 
     "YEAPR UNLWW TKVUL CPJAV BINDZ PTTNN HDTRS RKIVT YAEKL NOLDQ IUMOS TYKAM BHGLG YINTV "
     "LPHFA VPYAC PYLTX UDJLA QWTCN EPBWK MBTSP YNCES TTNYL GGAIY PUECO YSLWE EDKUI NAYTV XLNDU "
     "SSVTM TSPHA EJOAC OJOQF QELGC SHEIW PCQAP WFELG CS"; //shouldn't this be a vector, or is it fine as a string
 
-    string choice;
-    /* We probably do not need to write all 96 keys into a vector so I stopped at the 55 item(suck)
+    char choice;
+    string ownText;
+    int keyNum; //0 - 54
+    // We probably do not need to write all 96 keys into a vector so I stopped at the 55 item(suck)
     vector<string> keys = {"receipt", "vengeful" , "popcorn", "basketball", "friends", "wealth", "interrupt",
     "awesome", "actually", "kindhearted", "nifty", "change", "heat", "squeal", "treatment", "satisfy", "separate", "hot", "serious", 
     "pail", "request", "request", "different", "righteous", "cat", "cheap", "repair", "uncle", "creepy", "birds", "crook", "great",
     "ski", "stitch", "example", "flippant", "puzzling", "sleepy", "discover", "excuse", "unbiased", "jail", "eggs", "material",
     "expand", "equal", "group", "bizzare", "spotted", "embarrass", "produce", "womanly", "spectacular", "agonizing", "cows", "suck"};
-    */
+    
 
-    cout << "Would you like to decrypt the text(D) or encrypt your own(E). Please only enter E or D\n";
-    getline(cin , choice);
+    cout << "Would you like to decrypt the text(D) or encrypt your own(E). Please only enter E or D" << endl;
+    cin >> choice;
 
-
-    if(choice == "E"){
-
+    
+    if(choice == 'D'){
+        cout << "What is your key?";
+        cin >> keyNum;
+        cout << keys[keyNum];
+    }
+    else if(choice == 'E'){
+        cout << "What is your text";
+        getline(cin, ownText);
+        cout << ownText;
     }
     else{
-
+        cout << "Please only enter E or D";
     }
+
+
 
     return 0;
 }
